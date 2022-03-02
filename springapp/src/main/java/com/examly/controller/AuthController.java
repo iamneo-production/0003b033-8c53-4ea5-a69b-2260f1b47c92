9cf1e4fbb8e863c0d0fa223d61226b5dcb2bf3a0
Gaurav7u <gauravsingha32@gmail.com>
2022-03-02 12:11:53 +0000

    Added Signup Backend

---
+++ springapp/src/main/java/com/examly/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.examly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.examly.model.LoginModel;
+import com.examly.model.UserModel;
+import com.examly.repository.UserRepository;
+
+@CrossOrigin(origins = "https://8080-febbacaecaaeddbefefcfadeceafbaedad.examlyiopb.examly.io/")
+@RestController
+@RequestMapping("/user")
+public class AuthController {
+
+	@Autowired
+	private UserRepository urepo;
+	
+	@PostMapping("/signup")
+	public String saveUser(@RequestBody UserModel user) {
+		urepo.save(user);
+		return "\"User added\"";
+		
+	}
+	// @PostMapping("/login")
+	// public boolean isUserPresent(@RequestBody LoginModel data)
+	// {
+	// 	UserModel user=urepo.findByEmailAndPassword(data.getEmail(),data.getPassword());
+	// 	if(user==null)
+	// 		return false;
+	// 	else
+	// 		return true;
+		
+	// }
+}
